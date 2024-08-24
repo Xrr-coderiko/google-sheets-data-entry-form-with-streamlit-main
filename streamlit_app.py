860d5f33930a8cd53acfa84f1b08f0ad03648462
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-24 23:14:26 +0530

    Init 002 commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,21 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import re
-import gspread
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import tempfile
+
+SERVICE_ACCOUNT_FILE = 'secrets.toml'
+
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets"]
+)
+
+drive_service = build('drive', 'v3', credentials=credentials)
+sheets_service = build('sheets', 'v4', credentials=credentials)
+
 
 # Display Title and Description
 # st.image("./VOXlogo.jpeg",width=500,)
@@ -51,6 +65,18 @@ SIZES = [
 
 pattern = re.compile(r"^[6-9]\d{9}$")
 
+def upload_to_drive(file):
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        temp.write(file.getbuffer())
+        temp_path = temp.name
+    
+    file_metadata = {'name': file.name, 'parents': ['https://drive.google.com/drive/folders/1xX6CikQ3zUhwL-CLQSYWgc2GhAo-yeBg?usp=drive_link']}  # Replace with your Google Drive folder ID
+    media = MediaFileUpload(temp_path, resumable=True)
+    uploaded_file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    file_id = uploaded_file.get('id')
+    file_link = f"https://drive.google.com/file/d/{file_id}/view?usp=sharing"
+    return file_link
+
 # Onboarding New Vendor Form
 with st.form(key="vendor_form"):
     Name = st.text_input(label="Indent Raised By*")
@@ -71,7 +97,7 @@ with st.form(key="vendor_form"):
            size_list.append(st.selectbox(label="Size", options=SIZES, key=f'input_size{row}'))
        with grid[3]:
            quantity_list.append(st.text_input(label="Quantity", key=f'input_quantity{row}'))
-    Quantity = st.text_input(label="Quantity of panels*")
+    
     Dateofdisplay = st.date_input(label="Date of display executed*")
     InvoiceDoc = st.file_uploader(label="Upload Invoice copy*")
     DisplayImage = st.file_uploader(label="Upload Display images*")
@@ -97,6 +123,8 @@ with st.form(key="vendor_form"):
         elif Phone in existing_data["Phone"].astype(str).values:
             st.warning("Phone number already exists.")             
         else:
+            invoice_link = upload_to_drive(InvoiceDoc)
+            display_image_link = upload_to_drive(DisplayImage)
             # Create a new row of vendor data
             vendor_data = pd.DataFrame(
                 [
@@ -112,8 +140,8 @@ with st.form(key="vendor_form"):
                         "Sizes": ", ".join(size_list),
                         "Quantity": ", ".join(quantity_list),
                         "Display date": Dateofdisplay.strftime("%Y-%m-%d"),
-                        "Invoice": InvoiceDoc,
-                        "Display Image": DisplayImage, 
+                        "Invoice": invoice_link,
+                        "Display Image": display_image_link, 
                     }
                 ]
             )
