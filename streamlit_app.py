a9fa0eed9126bc1c57c8a6084c415447adae9835
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-25 00:29:54 +0530

    init 101 commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,10 +56,10 @@ with st.form(key="vendor_form"):
     Distributor = st.text_input(label="Distributor Name*")
     Dealer = st.text_input(label="Dealer Name*")
     City = st.text_input(label="City*")
-    num_rows = st.slider('Number of rows', min_value=1, max_value=10)
+    #num_rows = st.slider('Number of rows', min_value=1, max_value=10)
     grid = st.columns(4)
     product_list, color_list, size_list, quantity_list = [], [], [], []
-    for row in range(num_rows):
+    def addrow(row):
        with grid[0]:
            product_list.append(st.selectbox(label=f"Product {row+1}",index=None, options=PRODUCTS, key=f'input_product{row}'))
        with grid[1]:
@@ -68,6 +68,7 @@ with st.form(key="vendor_form"):
            size_list.append(st.selectbox(label="Size",index=None, options=SIZES, key=f'input_size{row}'))
        with grid[3]:
            quantity_list.append(st.text_input(label="Quantity", key=f'input_quantity{row}'))
+    AddButton = st.button(lable = 'Add more product', onClick=addrow)
     
     Dateofdisplay = st.date_input(label="Date of display executed*")
     InvoiceDoc = st.file_uploader(label="Upload Invoice copy*")
