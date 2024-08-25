36e1f30f88e834e4c02db0e833d8ebf61bdbe4de
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-25 16:53:34 +0530

    232 commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,6 @@ SIZES = [
 
 pattern = re.compile(r"^[6-9]\d{9}$")
 
-
 # Onboarding New Vendor Form
 with st.form(key="vendor_form"):
     Name = st.text_input(label="Indent Raised By*")
@@ -57,7 +56,7 @@ with st.form(key="vendor_form"):
     Distributor = st.text_input(label="Distributor Name*")
     Dealer = st.text_input(label="Dealer Name*")
     City = st.text_input(label="City*")
-    num_rows = st.text_input(label="Number of VOX Products")
+    num_rows = st.slider('Number of rows', min_value=1, max_value=10)
     grid = st.columns(4)
     product_list, color_list, size_list, quantity_list = [], [], [], []
     for row in range(num_rows):
@@ -69,6 +68,8 @@ with st.form(key="vendor_form"):
            size_list.append(st.selectbox(label="Size",index=None, options=SIZES, key=f'input_size{row}'))
        with grid[3]:
            quantity_list.append(st.text_input(label="Quantity", key=f'input_quantity{row}'))
+           
+    
     Dateofdisplay = st.date_input(label="Date of display executed*")
     InvoiceDoc = st.file_uploader(label="Upload Invoice copy*")
     DisplayImage = st.file_uploader(label="Upload Display images*")
@@ -77,8 +78,11 @@ with st.form(key="vendor_form"):
     # Mark mandatory fields
     st.markdown("**required*")
 
+
     submit_button = st.form_submit_button(label="Submit Details")
- 
+    
+
+
     # If the submit button is pressed
     if submit_button:
         # Check if all mandatory fields are filled
