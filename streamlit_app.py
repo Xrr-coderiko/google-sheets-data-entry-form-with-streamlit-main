2df6e30df782ed4b838dbaed5482fe718e99c743
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-26 15:59:26 +0530

    3354232

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ with st.form(key="vendor_form"):
     Distributor = st.text_input(label="Distributor Name*")
     Dealer = st.text_input(label="Dealer Name*")
     City = st.text_input(label="City*")
-    num_rows = st.number_input(label="Number of VOX Products", on_change=grid, min_value = 1, max_value = 10)
+    num_rows = st.number_input(label="Number of VOX Products", min_value = 1, max_value = 10)
     grid = st.columns(4)
     product_list, color_list, size_list, quantity_list = [], [], [], []
     for row in range(num_rows):
